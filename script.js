b5098faa17f458587624447db567c1000adf0e63
Rand Hale <randluca@gmail.com>
2020-12-22 18:52:25 -0800

    set up toggle function for viewing high scores and returning to th game

--- script.js
+++ script.js
@@ -2,15 +2,17 @@ var timeDisplay = document.getElementById("timer");
 var highScoreButton = document.getElementById("highScoreView");
 var startButton = document.getElementById("start-button");
 var mainBox = document.getElementById("main-content");
+var headBox = document.getElementById("header-content");
 
 var timeLeft = 100;
+var showMode = "game";
 
 // declaring the empty variable countdown on the global scope to serve as the target of setInterval, and to be accessible outside of function scope. 
 var countdown;
 
 function startGame(){
     // this line clears the HTML contents of <main> to prepare it for population by the quiz questions.
-    mainBox.innerHTML = '';
+    mainBox.innerHTML = "";
 
     countdown = setInterval(function(){
     timeLeft--;
@@ -29,7 +31,16 @@ function gameOver(){
 }
 
 function viewHighScores(){
-
+    if (showMode === "game"){
+        highScoreButton.textContent = "Back to main";
+        mainBox.setAttribute("class", "hidden");
+        showMode = "scores";
+    } else {
+        showMode = "game";
+        mainBox.setAttribute("class", "visible");
+        highScoreButton.textContent = "View High Scores";
+    }
+    
 }
 
 startButton.addEventListener("click", startGame)
