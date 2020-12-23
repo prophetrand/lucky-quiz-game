1981c28dd59c003fd80b9b4b1c27f35b188d603b
Rand Hale <randluca@gmail.com>
2020-12-22 20:36:04 -0800

    add question template with event listeners for answer choices

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 var timeDisplay = document.getElementById("timer");
 var highScoreButton = document.getElementById("highScoreView");
 var startButton = document.getElementById("start-button");
-var mainBox = document.getElementById("main-content");
 var headBox = document.getElementById("header-content");
+var mainBox = document.getElementById("main-content");
+var questionBox = document.getElementById("question-box");
+var questionText = document.getElementById("question-text");
+var answerEvent = document.getElementById("answer-event");
+
 
 var timeLeft = 100;
 var showMode = "game";
@@ -11,19 +15,21 @@ var showMode = "game";
 var countdown;
 
 function startGame(){
-    // this line clears the HTML contents of <main> to prepare it for population by the quiz questions.
+    // this line clears the HTML contents of <main> to prepare the page for population by the quiz questions in <section id="question-box"></section>
     mainBox.innerHTML = "";
 
     countdown = setInterval(function(){
-    timeLeft--;
-    timeDisplay.textContent = timeLeft;
+        timeLeft--;
+        timeDisplay.textContent = timeLeft;
 
-    if (timeLeft === 0){
-        clearInterval(countdown);
-        alert("Game Over! Score ZERO")
-        gameOver();
-    }
+        if (timeLeft === 0){
+            clearInterval(countdown);
+            alert("Game Over! Score ZERO")
+            gameOver();
+        }
     }, 1000);
+
+    runQuestion1();
 }
 
 function gameOver(){
@@ -33,11 +39,11 @@ function gameOver(){
 function viewHighScores(){
     if (showMode === "game"){
         highScoreButton.textContent = "Back to main";
-        mainBox.setAttribute("class", "hidden");
+        // make scores display
         showMode = "scores";
     } else {
         showMode = "game";
-        mainBox.setAttribute("class", "visible");
+        // make game come back
         highScoreButton.textContent = "View High Scores";
     }
     
@@ -45,3 +51,45 @@ function viewHighScores(){
 
 startButton.addEventListener("click", startGame)
 highScoreButton.addEventListener("click", viewHighScores)
+
+// questions will be housed in the section id="question-box", pointed w/ questionBox
+// each question's text will go into the <h2 id="question-text">, pointed w/ questionText.
+// each question will have four buttons whose textContent is each answer.
+
+function runQuestion1(){
+    questionText.textContent = "Your first question will be regarding the French language... Which of the following is the correct translation of \"A Pineapple\"?";
+    var answer1 = document.createElement("button");
+    var answer2 = document.createElement("button");
+    var answer3 = document.createElement("button");
+    var answer4 = document.createElement("button");
+
+    answerEvent.appendChild(answer1);
+    answerEvent.appendChild(answer2);
+    answerEvent.appendChild(answer3);
+    answerEvent.appendChild(answer4);
+
+    answer1.textContent = "un ananas";
+    answer2.textContent = "un pamplemousse";
+    answer3.textContent = "une poire";
+    answer4.textContent = "une pinape";
+
+    answerEvent.addEventListener("click", function(){
+        console.log("Voila, c'est magnifique")
+    });
+
+}
+
+
+
+
+// The following will be useful for creating a form for high score name entry, and was taken from 12-Ins_Preventing_Default_Events
+{/* <form >
+    <input placeholder="name" id="name" />
+    <input placeholder="email" id="email" />
+    <button id="submit">Submit</button>
+    <div>
+      <h4 id="response"></h4>
+    </div>
+</form> */}
+
+// Check here: https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp  to see how to make an element show/hide using no Bootstrap. Basically changing display property to/from "none"/"block". The element won't take up any space when it's set to display: "none" which i'll probably do with a .setAttribute() method.
